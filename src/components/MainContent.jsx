b2b46f183bf4d30baca7f0d40f0543ce86d976b1
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-02-20 09:55:37 +0000

    first commit

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+
+
+const MainContent = () => {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#e6e6e6', textAlign: 'center' }}>
+      <p>Welcome to my favorite cities app!</p>
+    </main>
+  );
+};
+
+export default MainContent;
